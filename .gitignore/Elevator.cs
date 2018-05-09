6abed0f70b3412a65cb761a32c07c19788878816
Timothy Seibel <7trseibel7@gmail.com>
2018-05-08 21:30:41 -0400

    Add files via upload

---
+++ .gitignore/Elevator.cs
@@ -0,0 +1,20 @@
+﻿namespace Final_Exam_Code
+{
+    class Elevator
+    {
+        private double MaxWeight;
+        private Occupants = Passenger array[];
+
+        public void(int maxOccupants + double maxWeight);
+        this.maxWeight = maxWeight()
+      }
+    {
+        Occupant = new Passenger;
+        Passenger p1 = new Occupant;
+    public Occupant(Passenger Passenger, int index);
+
+    public void();
+        Elevator.getCurrentWeight;
+        Elevator.isOverMaxCapacity;
+    }
+}
